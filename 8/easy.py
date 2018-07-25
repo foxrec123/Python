a793e7aa621f8a4332790eb22e7d2dfbd5470acd
foxrec123 <foxrec123@mail.ru>
2018-07-25 15:28:48 +0300

    generators

--- 8/easy.py
+++ 8/easy.py
@@ -18,23 +18,26 @@ def like_map(func, some_list):
 
 #like enumerate
 def like_enumerate(some_list):
-    index = 1
+    index = 0
     for item in some_list:
         yield index, item
         index += 1
-
+tuple
 #like_zip
 def like_zip(*args):
-    pass
+    for i in range(0, len(args[0])):
+        for_return = []
+        for item in args:
+            for_return.append(item[i])
+        yield tuple(for_return)
 
 #gen_inst = random_gen()
 #gen_inst = like_range(1, 100, 10)
 #gen_inst = like_map(lambda x: len(x), ['fuck', 'off', 'bastards'])
 #gen_inst = like_enumerate(['fuck', 'off', 'bastards'])
+gen_inst = like_zip('abc','123')
 
 i = iter(gen_inst)
 
 for _ in range(0, 3):
     print(next(i))
-
-zip()
